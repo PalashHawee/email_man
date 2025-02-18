d3ca330415bffd4239759f92586996866ce74fed
Palash Hawee <haweepalash@gmail.com>
2025-02-18 14:19:40 +0600

    ready for vercel

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,15 +13,15 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
-    ],
+    // "@typescript-eslint/array-type": "off",
+    // "@typescript-eslint/consistent-type-definitions": "off",
+    // "@typescript-eslint/consistent-type-imports": [
+    //   "warn",
+    //   {
+    //     "prefer": "type-imports",
+    //     "fixStyle": "inline-type-imports"
+    //   }
+    // ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
